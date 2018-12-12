b45239d431f93ba42c42ee0e02268c5b47341734
jwlian02 <jwlian02@gmail.com>
2018-12-12 15:53:51 -0500

    testing

--- app/src/main/java/com/example/razorback/calculator/MainActivity.java
+++ app/src/main/java/com/example/razorback/calculator/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //github testing...
         // not using "."; point or "%"; percent
         Button one = (Button) findViewById(R.id.one);
         Button two = (Button) findViewById(R.id.two);
