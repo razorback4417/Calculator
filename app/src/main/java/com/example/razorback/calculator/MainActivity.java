45bcc0f0b7cdbf55a84b7388fa04995276a035e4
razorback <razorback4417.800@gmail.com>
2019-01-02 15:29:35 -0500

    More edits

--- app/src/main/java/com/example/razorback/calculator/MainActivity.java
+++ app/src/main/java/com/example/razorback/calculator/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {//implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,28 +31,73 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button clear = (Button) findViewById(R.id.clear);
         Button equal = (Button) findViewById(R.id.equal);
         // Button point = (Button) findViewById(R.id.point);
-        TextView result1 = (TextView) findViewById(R.id.textView);
+        final TextView result1 = (TextView) findViewById(R.id.textView);
 
-    }
-    @Override
-    public void onClick(View v) {
-
-        switch (v.getId()) {
-
-            case R.id.one:
-                break;
-
-            case R.id.two:
-                // do your code
-                break;
-
-            case R.id.three:
-                // do your code
-                break;
-
-            default:
-                break;
-        }
-
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result1.setText(result1.getText() + "1");
+            }
+        });
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result1.setText(result1.getText() + "2");
+            }
+        });
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result1.setText(result1.getText() + "3");
+            }
+        });
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result1.setText(result1.getText() + "4");
+            }
+        });
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result1.setText(result1.getText() + "5");
+            }
+        });
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result1.setText(result1.getText() + "6");
+            }
+        });
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result1.setText(result1.getText() + "7");
+            }
+        });
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result1.setText(result1.getText() + "8");
+            }
+        });
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result1.setText(result1.getText() + "9");
+            }
+        });
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result1.setText(result1.getText() + "0");
+            }
+        });
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result1.setText(result1 = " ");
+            }
+        });
     }
 }
